7e0e66608912651a550addff8412e5af2301d5fe
Maggie Lau <maggieabcat@gmail.com>
2016-05-05 15:14:25 +0800

    hahaha

---
+++ hahaha/FirstTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FirstTableViewCell.swift
+//  hahaha
+//
+//  Created by Maggie Lau on 4/5/2016.
+//  Copyright © 2016 madmadgroup. All rights reserved.
+//
+
+import UIKit
+
+
+
+class FirstTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ hahaha/FirstViewController.swift
@@ -0,0 +1,59 @@
+//
+//  FirstViewController.swift
+//  hahaha
+//
+//  Created by Maggie Lau on 4/5/2016.
+//  Copyright © 2016 madmadgroup. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDelegate{
+    mag@IBOutlet var tableView : UITableView
+    var fruits: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fruits = ["Apple", "Pineapple", "Orange", "Blackberry", "Banana", "Pear", "Kiwi", "Strawberry", "Mango", "Walnut", "Apricot", "Tomato", "Almond", "Date", "Melon", "Water Melon", "Lemon", "Coconut", "Fig", "Passionfruit", "Star Fruit", "Clementin", "Citron", "Cherry", "Cranberry"]
+        
+        self.registerClass(FirstTableViewCell.self, forCellReuseIdentifier: "cell")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        let numberOfRows = fruits.count
+//        return numberOfRows
+//    }
+//
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+//        
+//        // Fetch Fruit
+//        let fruit = fruits[indexPath.row]
+//        
+//        // Configure Cell
+//        cell.textLabel?.text = fruit
+//        
+//        return cell
+//    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
